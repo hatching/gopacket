e3ba62860ba6c5e3695328a5e46217b1b469ec02
Jurriaan Bremer <jbr@hatching.io>
2019-03-11 05:38:35 +0100

    implement tls handshake server hello parser

--- layers/tls_handshake.go
+++ layers/tls_handshake.go
@@ -7,12 +7,36 @@
 package layers
 
 import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+
 	"github.com/google/gopacket"
+)
+
+type TLSHandshakeType uint8
+
+const (
+	TLSHandshakeHelloRequest       TLSHandshakeType = 0
+	TLSHandshakeClientHello        TLSHandshakeType = 1
+	TLSHandshakeServerHello        TLSHandshakeType = 2
+	TLSHandshakeCertificate        TLSHandshakeType = 11
+	TLSHandshakeServerKeyExchange  TLSHandshakeType = 12
+	TLSHandshakeCertificateRequest TLSHandshakeType = 13
+	TLSHandshakeServerHelloDone    TLSHandshakeType = 14
+	TLSHandshakeCertificateVerify  TLSHandshakeType = 15
+	TLSHandshakeClientKeyExchange  TLSHandshakeType = 16
+	TLSHandshakeFinished           TLSHandshakeType = 20
 )
 
 // TLSHandshakeRecord defines the structure of a Handshare Record
 type TLSHandshakeRecord struct {
 	TLSRecordHeader
+
+	HandshakeType   TLSHandshakeType
+	HandshakeLength int
+
+	ServerHello []TLSHandshakeServerHelloRecord
 }
 
 // DecodeFromBytes decodes the slice into the TLS struct.
@@ -22,7 +46,63 @@ func (t *TLSHandshakeRecord) decodeFromBytes(h TLSRecordHeader, data []byte, df 
 	t.Version = h.Version
 	t.Length = h.Length
 
-	// TODO
+	if len(data) != int(t.Length) {
+		return errors.New("TLS Handshake length mismatch")
+	}
 
+	// TODO Is this an error?
+	if t.Length < 4 {
+		return nil
+	}
+
+	t.HandshakeType = TLSHandshakeType(data[0])
+	t.HandshakeLength = int(data[1])*0x10000 + int(data[2])*0x100 + int(data[3])
+	if t.HandshakeLength+4 > len(data) {
+		df.SetTruncated()
+		return errors.New("TLS Handshake length mismatch")
+	}
+
+	switch t.HandshakeType {
+	case TLSHandshakeServerHello:
+		var r TLSHandshakeServerHelloRecord
+		err := r.decodeFromBytes(data[4:4+t.HandshakeLength], df)
+		if err != nil {
+			return err
+		}
+		t.ServerHello = append(t.ServerHello, r)
+	default:
+		// TODO More Handshake handlers.
+		return fmt.Errorf("TLS Handshake message not implemented yet: %d", t.HandshakeType)
+	}
+	return nil
+}
+
+type TLSHandshakeServerHelloRecord struct {
+	Version           uint16
+	Random            []byte
+	SessionId         []byte
+	CipherSuite       uint16
+	CompressionMethod uint8
+
+	// TODO Extensions
+}
+
+func (t *TLSHandshakeServerHelloRecord) decodeFromBytes(data []byte, df gopacket.DecodeFeedback) error {
+	if len(data) < 38 {
+		df.SetTruncated()
+		return errors.New("TLS Handshake ServerHello packet length mismatch")
+	}
+
+	t.Version = binary.BigEndian.Uint16(data[:2])
+	t.Random = data[2:34]
+	length := int(data[34])
+	if length+38 > len(data) {
+		df.SetTruncated()
+		return errors.New("TLS Handshake ServerHello packet length mismatch")
+	}
+
+	t.SessionId = data[35 : length+35]
+	t.CipherSuite = binary.BigEndian.Uint16(data[35+length : 37+length])
+	t.CompressionMethod = data[length+37]
 	return nil
 }
